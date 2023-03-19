322cb69d501496bf235350bbbd179ff327ddb1e2
MdAhnafArshad <ahnafarshad.me@gmail.com>
2023-03-19 19:14:11 +0600

    img slider

---
+++ index.js
@@ -0,0 +1,26 @@
+// global variables
+let indexCounter = 0;
+const images = [
+    'images/img-1.jpg',
+    'images/img-2.jpg',
+    'images/img-3.jpg',
+    'images/img-4.jpg',
+    'images/img-5.jpg',
+    'images/img-6.jpg',
+]
+
+const imgEL = document.getElementById('slider-img');
+
+// DOM elements
+const intervalId = setInterval(() => {
+    if(indexCounter == 5) {
+        indexCounter = 0;
+    }
+    const imgUrl = images[indexCounter];
+    imgEL.setAttribute('src', imgUrl);
+    indexCounter++;
+    
+    
+}, 1000);
+
+
